f24487ac3557841810c2267bd93e9ff24cfcc419
Berengere <berengere.pelletier@gmail.com>
2016-06-02 11:46:02 +0200

    boucle for et coucou

--- Testgit/src/Gigit.java
+++ Testgit/src/Gigit.java
@@ -3,6 +3,11 @@ public class Gigit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Coucou");
+			
+		}
 	}
 
 }
